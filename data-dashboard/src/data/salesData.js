55cf58938329465d090c8695107aab33b2676a37
raaaj1903 <rj471529@gmail.com>
2026-02-14 22:28:05 +0530

    add

---
+++ data-dashboard/src/data/salesData.js
@@ -0,0 +1,8 @@
+export const salesData = [
+  { month: "Jan", sales: 400 },
+  { month: "Feb", sales: 600 },
+  { month: "Mar", sales: 350 },
+  { month: "Apr", sales: 800 },
+  { month: "May", sales: 700 },
+  { month: "Jun", sales: 500 },
+];
